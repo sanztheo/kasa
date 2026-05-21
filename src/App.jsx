ebcae03464f434fc5949958179eb37f8ea78834d
Sanzcloud-web <sanztheopro@gmail.com>
2026-05-21 15:02:59 +0200

    Add Header and Footer components to App; update layout structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,22 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import HomePage from './pages/home/HomePage';
 import AboutPage from './pages/about/AboutPage';
 import DetailPage from './pages/detail/DetailPage';
-
 import NotFoundPage from './pages/error/NotFoundPage';
+
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+
 export default function App() {
   return (
     <BrowserRouter>
-      // import header
+      <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/detail/:id" element={<DetailPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
-      // import footer 
+      <Footer />
     </BrowserRouter>
   );
 }
